85742f211f8f06194e3f286b91bd643d118afc4d
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-08-31 14:30:02 +0000

    ADD: Header component
    Header component demonstrate the flexbox playground

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.scss';
 import Box from './components/box/box';
 import Controls from './components/controls/controls';
 import Screen from './components/screen/screen';
+import Header from './layout/header/header'
 function App() {
   return (
     <div className="app">
+      <Header/>
       <Screen />
       <Controls />
     </div>
---
+++ src/layout/header/header.jsx
@@ -0,0 +1,11 @@
+import './header.sass'
+const Header = ()=>{
+    return(
+        <header className='header'>
+            <h1>Flexbox Playground</h1>
+            <p>A Visualizer tool to demonstrate how CSS flex truly functions.</p>
+        </header>
+    )
+}
+
+export default Header;
