f62a96357f375a83b9b690df98b02d716c4b89b5
Ak-ram <ashrfakrm64@gmail.com>
2022-08-31 01:15:08 +0000

    Modify: Select component

--- src/components/select/select.jsx
+++ src/components/select/select.jsx
@@ -1,21 +1,32 @@
 import { useRef } from 'react';
-import { useState } from 'react'
+import { useState } from 'react';
+import { MdOutlineCheckCircleOutline } from 'react-icons/md';
+
 import './select.sass'
 const Select = ({ name, options }) => {
     const [isClicked, setClicked] = useState(false);
-    const openOptionsList = ()=>{
-        let options= document.querySelectorAll('.secondary-options');
-        for(let i=0; i<options.length;i++)
+    const ChangeOptionsListStatus = () => {
+        let options = document.querySelectorAll('.secondary-options');
+        for (let i = 0; i < options.length; i++)
             options[i].classList.add('hide')
-        setClicked(!isClicked)    
+        setClicked(!isClicked);
     }
+
+
     return (
         <div className='select'>
             <div className='name'>{name} :</div>
             <div className='options'>
-                <div className='default-option' onClick={openOptionsList}>row</div>
+                <div className='default-option' onClick={ChangeOptionsListStatus}>row</div>
                 <div className={`${isClicked ? 'secondary-options' : 'secondary-options hide'}`}>
-                    {options?.map(option => <div key={option} className="option"><span className="checkbox"></span><span className='_option'>{option}</span></div>)}
+                    {options?.map((option,index) => {
+                        return(
+                            <div key={option} className="option" onClick={ChangeOptionsListStatus}>
+                            {<MdOutlineCheckCircleOutline color={'#bbb'} size={22} />}
+                            <span className='_option'>{option}</span>
+                        </div>
+                        )
+                    })}
                 </div>
             </div>
         </div>
