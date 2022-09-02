5bc7ab8732ed967fd9e54261d230220cc8f61b45
Akram Ashraf <ashrfakrm64@gmail.com>
2022-09-02 10:44:40 +0200

    Fix: selectbox issue

--- src/components/select/select.jsx
+++ src/components/select/select.jsx
@@ -4,7 +4,7 @@ import Option from '../option/option'
 import './select.sass'
 const Select = ({ name, prop }) => {
     const [isClicked, setClicked] = useState(false);
-    // const [isOpened, setOpened] = useState(false);
+    const [isOpened, setOpened] = useState(false);
     const [options,setOptions] = useState([])
     try{
         useEffect(()=>{
@@ -16,15 +16,15 @@ const Select = ({ name, prop }) => {
         for (let i = 0; i < optionElement.length; i++)
             optionElement[i].classList.add('hide')
         setClicked(!isClicked);
-        // setOpened(!isOpened);
+        setOpened(!isOpened);
     }
     return (
         <div className='select'>
             <div className='name'>{name}</div>
             <div className='options'>
                 <div className='default-option' onClick={(ele)=>ChangeOptionsListStatus(ele)}>
-                    {/* Choose {isOpened?<IoIosArrowDown />:<IoIosArrowUp />} */}
-                    select
+                    Choose {isOpened?<IoIosArrowDown />:<IoIosArrowUp />}
+                    {/* select */}
                     </div>
                 <div className={`${isClicked ? 'secondary-options' : 'secondary-options hide'}`}>
                     {options?.map((option) => {
